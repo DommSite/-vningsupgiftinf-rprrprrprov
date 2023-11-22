35564a13695f3f7d479a03c8610a25b9f1692080
DommSite <johan.johansson06@live.se>
2023-11-22 09:59:48 +0100

    Add project files.

---
+++ övningsupgiftinförprrprrprov/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace testestestest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            for (int i = 0; i < namn.Length; i++) 
+            {
+                Console.WriteLine("Skriv namn nummer " + (i+1));
+                namn[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Dina fem namn var: ");
+            for(int i = 0; i < namn.Length; i++) 
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
